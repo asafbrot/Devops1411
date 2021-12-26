e1c17f1a8e935fcab25acc42920ac272abf2a5de
asafbrot <asafbrot@gmail.com>
2021-12-26 18:39:23 +0200

    add 15.py

---
+++ 15.py
@@ -0,0 +1,50 @@
+#a = [1,2,3,4,5]
+#for number in a:
+  #  print("hello world!")
+
+#range (begin, end, step
+#print(list(range(9, 2, -1)))
+#what_to_print = "hello world!"
+#amount = 4
+#list_of_names = ["noam","asaf", "lior", "moshe" , "hen"]
+
+#i is index. position number inside the list
+#for i in range(amount):
+ #  print(str(i) + " " + what_to_print)
+
+#for i in range(len(list_of_names)):
+#    print(list_of_names[i])
+
+#break - end loop
+#for name in list_of_names:
+#    if name == "hen":
+#        continue
+#    else:
+#      print(name)
+
+#a = 0
+
+#
+
+
+
+num = 1
+
+#while num <= 100:
+ #   if num % 7 == 0 or "7" in str(num):
+ #       num = num + 1
+  #      continue
+
+#    else:
+ #       print(num)
+ #       num = num + 1
+
+#option b
+
+a = "a"
+
+while a != "q":
+    a = input("enter q or not : ")
+else:
+    print("finished successfully ")
+
