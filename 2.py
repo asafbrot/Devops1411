be5b9401fb7d76e7f0292ac5cb55eaa02d4458f1
asafbrot <96367100+asafbrot@users.noreply.github.com>
2021-12-26 19:19:26 +0200

    add 2.py (#1)
    * add 2.py
    
    * Create 334.py

---
+++ 2.py
@@ -0,0 +1,16 @@
+my_number = "4"
+result = float(my_number) -2
+print(result)
+#print(f"your result is "  {result})
+
+my_list = [10, 20, 30, 40, 50, 60, 70, 80]
+print(my_list[::-1])
+my_str = "hello and welcome to \\\"devops experts\\\""
+print(my_str)
+
+my_dict = {"fname": "asaf", "lname": "brot", "age": "41",  "id": "040426249"}
+print (my_dict.values())
+
+key_to_print = input(f"enter key : {list(my_dict.keys())}")
+print("you chose to print: " + str(my_dict[key_to_print]))
+
--- 334.py
+++ 334.py
@@ -1,1 +1,14 @@
-print("conflict")
+my_file = open("read_my_contents.txt")
+
+for line in my_file.readlines():
+    print(line, end='')
+
+my_other_file = open("moshe.txt", "w")
+my_other_file.write("hey hey\n")
+
+# "w" - defines file as "Write" - means he create the file as blank and put whatever you want in it
+# - if not mentioned - default is read
+
+
+
+
