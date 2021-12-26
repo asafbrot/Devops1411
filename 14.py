021d4e4d6429ca509d6fb28d2503ec01c1f3c9e9
asafbrot <asafbrot@gmail.com>
2021-12-26 18:36:51 +0200

    add 14.py

---
+++ 14.py
@@ -0,0 +1,36 @@
+isTrue = False
+a = 2
+b = 2.5
+strone = "one"
+strthree = "three"
+
+if a >= 2 and strone == "one" or not strthree == "two":
+    print("a is greater than 2")
+elif b == 2.5 or b == 2.5:
+    print("something")
+elif strone == "3":
+    print("blablabla")
+else:
+    print("a is lower than 2")
+
+my_list = ["hen", "lior", "shay", "ariel"]
+if "hen" in my_list:
+    print("hen")
+
+my_other_list = []
+
+#check if list is empty.if there is something in the list - it will print hello
+if my_other_list:
+    print("hello")
+
+if 0 < a <= 4:
+    print("a is between 0 and 4")
+
+if len(my_list) > 0:
+    print("hey")
+
+c = ["aaa", "1"]
+d = ["aaa", "1"]
+e = 9
+if type(e) is int:
+    print("e is an integer")
